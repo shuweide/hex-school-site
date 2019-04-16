25e35663646fae7023dd249599d145ed88f64f32
shuweide <shuweide@gmail.com>
2019-04-16 17:48:52 +0800

    js ch6

--- js/ch5.js
+++ js/ch5.js
@@ -1,19 +1,24 @@
+let menu = {
+  ham : {
+    price : 50,
+    cal : 600
+  },
+  coke : {
+    price : 20,
+    cal: 100
+  }
+};
+
+function getPrice (num, price) {
+  return price * num;
+}
+
 document.getElementById('countId').onclick = function () {
   let hamNum = document.getElementById('hamNumId').value;
   let cokeNum = document.getElementById('cokeNumId').value;
   console.log('hamNum is ' + typeof (hamNum));
   console.log('cokeNum is ' + typeof (cokeNum));
   document.getElementById('totalId').textContent =
-    getHamPrice(parseInt(hamNum)) + getCokePrice(parseInt(cokeNum));
+    getPrice(hamNum, menu.ham.price)+ getPrice(cokeNum, menu.coke.price);
 };
-
-function getHamPrice (num) {
-  let hamPrice = 50;
-  return hamPrice * num;
-}
-
-function getCokePrice (num) {
-  let cokePrice = 20;
-  return cokePrice * num;
-}
 
