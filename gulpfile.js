10ddb37d741c26292b7852f8de932cd9fc2453f1
shuwei.kuo <shuweide@gmail.com>
2019-05-31 01:38:49 +0800

    gulp sass test

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 let gulp = require('gulp');
 let jade = require('gulp-jade');
+let sass = require('gulp-sass');
 
 gulp.task('copyHTML', function () {
   return gulp.src('./*.html')
@@ -7,9 +8,24 @@ gulp.task('copyHTML', function () {
 });
 
 gulp.task('jade',function () {
-  return gulp.src('./gulp/**/*.jade')
+  return gulp.src('./jade/**/*.jade')
     .pipe(jade({
       pretty:true
     }))
     .pipe(gulp.dest('./public/'))
 });
+
+gulp.task('sass', function(){
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./public/css'));
+});
+
+//監控Sass
+gulp.task('watch', function(){
+  gulp.watch('./sass/**/*.scss', gulp.series('sass'));
+  gulp.watch('./jade/**/*.jade', gulp.series('jade'));
+});
+
+//合併task
+gulp.task('default', gulp.series('jade','sass','watch'));
