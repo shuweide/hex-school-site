adf826ba092bea97689803efd283cd6d196e4cc7
shuwei.kuo <shuweide@gmail.com>
2019-06-01 03:12:12 +0800

    test gulp plugin concat babel autoprefixer plumber etc

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,56 @@
-let gulp = require('gulp');
-let jade = require('gulp-jade');
-let sass = require('gulp-sass');
+const gulp = require('gulp');
+const $ = require('gulp-load-plugins')(); //針對gulp開頭的套件作自動管理
+// let jade = require('gulp-jade');
+// let sass = require('gulp-sass');
+// let plumber = require('gulp-plumber');
+// let postcss = require('gulp-postcss');
+// const babel = require('gulp-babel');
+const autoprefixer = require('autoprefixer');
 
 gulp.task('copyHTML', function () {
   return gulp.src('./*.html')
+    .pipe($.plumber())
     .pipe(gulp.dest('./public/'))
 });
 
-gulp.task('jade',function () {
+gulp.task('jade', function () {
   return gulp.src('./jade/**/*.jade')
-    .pipe(jade({
-      pretty:true
+    .pipe($.plumber())
+    .pipe($.jade({
+      pretty: true
     }))
     .pipe(gulp.dest('./public/'))
 });
 
-gulp.task('sass', function(){
+gulp.task('sass', function () {
+
+  let plugins = [
+    autoprefixer({ browsers: ['last 1 version'] })
+  ];
+
   return gulp.src('./sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
+    .pipe($.plumber())
+    .pipe($.sass().on('error', $.sass.logError))
+    //編譯完成 CSS
+    .pipe($.postcss(plugins))
     .pipe(gulp.dest('./public/css'));
 });
 
+gulp.task('babel', () =>
+  gulp.src('./js/**/*.js')
+    .pipe($.plumber())
+    .pipe($.babel({
+      presets: ['@babel/env']
+    }))
+    .pipe($.concat('all.js')) //把全部js合併成一個檔案
+    .pipe(gulp.dest('./public/js'))
+);
+
 //監控Sass
-gulp.task('watch', function(){
+gulp.task('watch', function () {
   gulp.watch('./sass/**/*.scss', gulp.series('sass'));
   gulp.watch('./jade/**/*.jade', gulp.series('jade'));
 });
 
 //合併task
-gulp.task('default', gulp.series('jade','sass','watch'));
+gulp.task('default', gulp.series('jade', 'sass', 'babel', 'watch'));
